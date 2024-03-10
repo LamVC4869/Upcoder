9612b2d970495c63ee453d0dda55607da225b1eb
LamVC4869 <lamcity4869@gmail.com>
2024-03-10 22:36:04 +0700

    conflic

--- C++ Nâng Cao/CodeLearn/bit/bit1.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int main () {
-    int a; cin >> a;
-    cout << "a is an " << (a & 1 ? "odd number" : "even number");
-    return 0;
-}
--- lab/bai3l.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-    int x;
-    cin>>x;
-    int t=x%10;
-    cout<<t*10+(x-t)/10;
-    return 0;
-}
